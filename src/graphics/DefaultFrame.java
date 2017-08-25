2ea2749c9d821968897bd08f15e4450f56e139d0
Mamone-Gianluigi <mamonegianluigi@gmail.com>
2017-08-25 11:53:09 +0200

    Aggiunta riduzione icona e movimento finestra per il frame di errore e conferma

--- src/graphics/DefaultFrame.java
+++ src/graphics/DefaultFrame.java
@@ -11,6 +11,7 @@ import javax.swing.border.*;
 public class DefaultFrame extends JFrame {
 
 	private static final long serialVersionUID = 1L;
+	private static Point point = new Point();
 
 	/**
 	 * Crea un nuovo frame inserendo in esso la componente specificata.
@@ -36,8 +37,23 @@ public class DefaultFrame extends JFrame {
 		setTitle(title);
 		
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setResizable(false);
+		//setResizable(false);
 		setUndecorated(true);
+		
+		addMouseListener(new MouseAdapter() {
+            public void mousePressed(MouseEvent e) {
+                point.x = e.getX();
+                point.y = e.getY();
+            }
+        });
+		addMouseMotionListener(new MouseMotionAdapter() {
+            public void mouseDragged(MouseEvent e) {
+                Point p = getLocation();
+         
+                setLocation(p.x + e.getX() - point.x,
+                        p.y + e.getY() - point.y);
+            }
+        });
 		
 		componente.setFont(new Font("Georgia", Font.PLAIN, 18));
 		componente.setForeground(Color.BLACK);
@@ -89,13 +105,67 @@ public class DefaultFrame extends JFrame {
 		pannello.add(pannello1);
 		pannello.add(pannello2);
 		
+		
+		/*JButton dee=new JButton();
+		dee.setBorder(null);
+		dee.setContentAreaFilled(false);
+		ImageIcon ii=new ImageIcon(getClass().getResource("/resource/x.png"));
+		dee.setIcon(ii);
+		*/
+		
+		JButton dee1=new JButton();
+		dee1.setBorder(null);
+		dee1.setContentAreaFilled(false);
+		ImageIcon ii1=new ImageIcon(getClass().getResource("/resource/_.png"));
+		dee1.setIcon(ii1);
+		
+		JPanel drf=new JPanel();
+		drf.setLayout(new BorderLayout());
+		//drf.add(dee,BorderLayout.EAST);
+		drf.add(dee1,BorderLayout.EAST);
+		
+	/*	class ExitListener implements ActionListener {
+			public void actionPerformed(ActionEvent e) {
+				inserisciRegistra.dispose();
+				sistema.SvuotaParola();
+				conferma=conferma();
+				conferma.setVisible(true);
+			}
+		}*/
+		
+		class RiduciListener implements ActionListener {
+			public void actionPerformed(ActionEvent e) {
+		
+				int state = getExtendedState();
+				 
+				state = Frame.ICONIFIED;
+				setExtendedState(state);
+			}
+		}
+		
+	//	dee.addActionListener(new ExitListener());
+		dee1.addActionListener(new RiduciListener());
+		
+		JLabel label =new JLabel("Errore");
+		label.setFont(new Font("Georgia", Font.PLAIN, 18));
+		label.setHorizontalAlignment(JLabel.CENTER);
+		label.setForeground(Color.BLACK);
+		
+		JPanel drf1=new JPanel();
+		drf1.setLayout(new BorderLayout());
+		drf1.add(label,BorderLayout.CENTER);
+		drf1.add(drf,BorderLayout.EAST);
+		
+		
+		
 		TitledBorder t=new TitledBorder(new EtchedBorder(), title);
 		t.setTitleFont(new Font("Georgia", Font.ITALIC, 13));
 		t.setTitleColor(Color.BLACK);
 		pannello.setBorder(t);
 		JPanel tutto=new JPanel();
 		tutto.setLayout(new BorderLayout());
-		tutto.setBorder(BorderFactory.createLineBorder(Color.black,2));		
+		tutto.setBorder(BorderFactory.createLineBorder(Color.black,2));	
+		tutto.add(drf1,BorderLayout.NORTH); //	
 		tutto.add(pannello,BorderLayout.CENTER);
 		add(tutto);
 		
